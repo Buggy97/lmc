c279749c18d81bcf160b6bf5264137067fbc1cc2
Farjad Ali <dajraf2@gmail.com>
2018-11-25 23:16:27 +0100

    Merge with parser

--- lmc.pl
+++ lmc.pl
@@ -4,12 +4,12 @@
 
 /*EXECUTION LOOP*/
 execution_loop(State, Out) :- functor(State, halted_state, _),
-							  arg(5, State, Out).				 
-							  
-execution_loop(state(Acc, Pc, Mem, In, Out, Flag), F) :-						 
-				one_instruction(state(Acc, Pc, Mem, In, Out, Flag), NextState),	
-				execution_loop(NextState, F).						  
-							  
+							  arg(5, State, Out).
+
+execution_loop(state(Acc, Pc, Mem, In, Out, Flag), F) :-
+				one_instruction(state(Acc, Pc, Mem, In, Out, Flag), NextState),
+				execution_loop(NextState, F).
+
 /**********************************DEBUG******************************************
 dbexecution_loop(State, State) :- functor(State, halted_state, 6).				 *
 																				 *
@@ -19,11 +19,11 @@ dbexecution_loop(state(Acc, Pc, Mem, In, Out, Flag), F) :-						 *
 ****************************************************************************^****/
 /*OVERHEAD*/
 
-one_instruction(halted_state(Acc, Pc, Mem, In, Out, Flag), 
+one_instruction(halted_state(Acc, Pc, Mem, In, Out, Flag),
 				halted_state(Acc, Pc, Mem, In, Out, Flag)) :- !.
-				
+
 /*ADD*/
-one_instruction(state(Acc, Pc, Mem, In, Out, _), State) :- 
+one_instruction(state(Acc, Pc, Mem, In, Out, _), State) :-
 				nth0(Pc, Mem, INST),
 				split_instr(INST, OP, ARG),
 				OP = '1',
@@ -33,9 +33,9 @@ one_instruction(state(Acc, Pc, Mem, In, Out, _), State) :-
 				copy_term(state(Acc__, Pc_, Mem, In, Out, Flag_), State),
 				Pc_ \= 0,
 				!.
-				
-/*SUB*/			
-one_instruction(state(Acc, Pc, Mem, In, Out, _), State) :- 
+
+/*SUB*/
+one_instruction(state(Acc, Pc, Mem, In, Out, _), State) :-
 				nth0(Pc, Mem, INST),
 				split_instr(INST, OP, ARG),
 				OP = '2',
@@ -45,20 +45,20 @@ one_instruction(state(Acc, Pc, Mem, In, Out, _), State) :-
 				copy_term(state(Acc__, Pc_, Mem, In, Out, Flag_), State),
 				Pc_ \= 0,
 				!.
-				
+
 /*STORE*/
-one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :- 
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				split_instr(INST, OP, ARG),
 				OP = '3',
 				Pc_ is (Pc + 1) mod  100,
-				replace(ARG, Acc, Mem_),
+				replace_pos(Mem, ARG, Acc, Mem_),
 				copy_term(state(Acc, Pc_, Mem_, In, Out, Flag), State),
 				Pc_ \= 0,
 				!.
-				
+
 /*LOAD*/
-one_instruction(state(_, Pc, Mem, In, Out, Flag), State) :-  			
+one_instruction(state(_, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				split_instr(INST, OP, ARG),
 				OP = '5',
@@ -67,9 +67,9 @@ one_instruction(state(_, Pc, Mem, In, Out, Flag), State) :-
 				copy_term(state(Acc_, Pc_, Mem, In, Out, Flag), State),
 				Pc_ \= 0,
 				!.
-				
+
 /*BRANCH*/
-one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :- 
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				split_instr(INST, OP, ARG),
 				OP = '6',
@@ -79,7 +79,7 @@ one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 
 /*BRANCHIZ*/
 /*TRUE*/
-one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :- 
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				split_instr(INST, OP, ARG),
 				OP = '7',
@@ -88,8 +88,8 @@ one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				Pc_ is (ARG) mod 100,
 				copy_term(state(Acc, Pc_, Mem, In, Out, Flag), State),
 				!.
-/*FALSE*/		
-one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :- 
+/*FALSE*/
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				split_instr(INST, OP, _),
 				OP = '7',
@@ -100,7 +100,7 @@ one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 /*BRANCHIP*/
 
 /*TRUE*/
-one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :- 
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				split_instr(INST, OP, ARG),
 				OP = '8',
@@ -108,8 +108,8 @@ one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				Pc_ is (ARG) mod  100,
 				copy_term(state(Acc, Pc_, Mem, In, Out, Flag), State),
 				!.
-/*FALSE*/	
-one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :- 
+/*FALSE*/
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				split_instr(INST, OP, _),
 				OP = '8',
@@ -117,9 +117,9 @@ one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				Pc_ is (Pc + 1) mod  100,
 				copy_term(state(Acc, Pc_, Mem, In, Out, Flag), State),
 				!.
-				
-/*INPUT*/		
-one_instruction(state(_, Pc, Mem, In, Out, Flag), State) :- 
+
+/*INPUT*/
+one_instruction(state(_, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				string_chars(INST, [X, Y, Z|_]),
 				X = '9', Y = '0', Z = '1',
@@ -131,8 +131,8 @@ one_instruction(state(_, Pc, Mem, In, Out, Flag), State) :-
 				Pc_ \= 0,
 				!.
 
-/*OUTPUT*/				
-one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :- 
+/*OUTPUT*/
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				string_chars(INST, [X, Y, Z|_]),
 				X = '9', Y = '0', Z = '2',
@@ -142,12 +142,35 @@ one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				!.
 
 /*HALT*/
-one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :- 
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag), State) :-
 				nth0(Pc, Mem, INST),
 				atom_chars(INST, [X]), X = '0',
 				copy_term(halted_state(Acc, Pc, Mem, In, Out, Flag), State),
-				!.				
-				
+				!.
+
+/*Operazioni supportate*/
+oP("ADD", 1).
+oP("SUB", 2).
+oP("STA", 3).
+oP("LDA", 5).
+oP("BRA", 6).
+oP("BRZ", 7).
+oP("BRP", 8).
+oP("INP", 901).
+oP("OUT", 902).
+oP("HLT", 0).
+/*TODO: aggiungi oP("DAT", 42).*/
+
+/*parser*/
+parseLine([X|T], L) :- opp(X).
+
+/*Per OP VAL supponendo che le LBL siano tradotte*/
+opp([X|T], OPCode, Arg) :- validOp(X, OPCode),
+				                   validVal(T, Arg).
+
+validOp(X, OPCode) :- string_upper(X, Ux),
+				              oP(Ux, OPCode).
+
 /*Utils*/
 /*Rimuove l'ultimo elemento da una lista*/
 remove_last([], [], "") :- !.
@@ -158,18 +181,66 @@ remove_last([H|T], [H|S], F) :- remove_last(T, S, F),
 split_instr(Inst, X, Arg) :- string_chars(Inst, [X|T]),
 							 string_chars(Arg_, T),
 							 atom_number(Arg_, Arg).
-				
+
 /*Verifica che il valore è positivo e modulo 1000, se il valore non
  *Rispetta le condizioni allora restituisce flag altrimenti noflag
  */
 normalize(Val, NewVal, Flag) :- ((Val > 999; Val < 0),
-								NewVal is Val mod 1000, 
+								NewVal is Val mod 1000,
 								atom_string(Flag, "flag")), !;
 								NewVal is Val, atom_string(Flag, "noflag"), !.
-								
-/*Sostituisce un valore in una posizione*/
-replace(_, _, [], []).
-replace(O, R, [O|T], [R|T2]) :- replace(O, R, T, T2).
-replace(O, R, [H|T], [H|T2]) :- H \= O, replace(O, R, T, T2).
-								
-								
+
+/*Elimina un commento da una riga*/
+purge_comm(X, New) :- sub_string(X, Z, _, _, " //"),
+                      sub_string(X, 0, Z, _, New), !.
+purge_comm(X, New) :- sub_string(X, Z, _, _, "//"),
+                      sub_string(X, 0, Z, _, New), !.
+
+/*Elimina tutti i commenti data una lista di righe*/
+purge_comms([], []) :- !.
+purge_comms([X|S], [H|T]) :- purge_comm(X, H),
+                             H \= "",
+                             purge_comms(S, T),
+                             !.
+
+purge_comms([X|S], T) :- purge_comm(X, H),
+                         H = "",
+                         purge_comms(S, T),
+                         !.
+
+/*Sostituisce le occorrenze in lista*/
+replace([], _, _, []) :- !.
+replace([H|T], I, F, [H|S]) :- H \= I,
+                               replace(T, I, F, S),
+                               !.
+replace([H|T], I, F, [F|S]) :- H = I,
+                               replace(T, I, F, S),
+                               !.
+
+/*Sotituisce l'elemento in una determinata posizione*/
+replace_pos([_|T], 0, X, [X|T]).
+replace_pos([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace_pos(T, NI, X, R), !.
+replace_pos(L, _, _, L).
+
+
+/*Traduce le etichette in indirizzi*/
+
+
+/*Trova la posizione dell'etichetta nel codice*/
+find_pos(OG, [H|T], LBL, ADDR) :- split_string(H, " ", " ", [X|S]),
+                                  X = LBL,
+                                  nth0(ADDR, OG, H),
+                                  !.
+
+find_pos(OG, [H|T], LBL, ADDR) :- split_string(H, " ", " ", [X|S]),
+                                  X \= LBL,
+                                  find_pos(OG, T, LBL, ADDR),
+                                  !.
+
+/*Legge le linee da un file*/
+readLines(File, L) :- open(File, read, Stream),
+				              read_stream_to_codes(Stream, Codes),
+				              close(Stream),
+				              atom_codes(String, Codes),
+				              string_upper(String, Res),
+				              split_string(Res, "\n", "\n", L).
---
+++ test.asm
@@ -0,0 +1,15 @@
+
+
+//Ciao
+
+aDd 5 //Ciao
+
+
+
+
+//Ciao LOAD 2
+LMA //Ciao LD //Ciao 5
+
+
+
+LD//Ciao LMA
