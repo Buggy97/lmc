b3cc43237076117fe3b30346711bbf6b11eccabd
Farjad Ali <f.ali1@campus.unimib.it>
2019-01-08 23:58:53 +0100

    Added basic check for assembly source on lisp

--- lmc-lisp/lol.lisp
+++ lmc-lisp/lol.lisp
@@ -25,10 +25,15 @@
           ((string= inst "HLT") "0")))
 
 (defun isLabel (arg)
-    (and (eq (parse-integer (subseq arg 0 1) :junk-allowed T) nil) (not (getOp arg))))
+    (and (eq (parse-integer 
+        (subseq arg 0 1) :junk-allowed T) nil) (not (getOp arg))))
 
 (defun isValue (arg)
-    (eq (length arg) (length (write-to-string (parse-integer arg :junk-allowed T)))))
+    (or (integerp arg) 
+        (eq (length arg) (length (write-to-string 
+         (if (parse-integer arg :junk-allowed T)
+             (parse-integer arg :junk-allowed T)
+             0))))))
 
 (defun isValidArg (arg)
     (or (isLabel arg) (isValue arg)))
@@ -51,21 +56,23 @@
                 (if pos (write-to-string pos) nil))
              arg) nil))
 
-(defun DBLines (file) (getCleanLines (with-open-file (stream file :direction :input) (read-lines stream))))
+(defun DBLines (file) (getCleanLines 
+(with-open-file (stream file :direction :input) (read-lines stream))))
 
 (defun translate (insts insts2)
     (if insts
         (let ((inst (first (remove "" (split-sequence " " (first insts))
                                      :test #'equal))))
+             (write insts)
              (if (getOp inst) 
                 (cons (write-to-string (parse-integer (concatenate 
                                                         'string (getOp inst)
                                            (if (second 
                                                      (remove "" (split-sequence
                                                                  " "
-                                                                     (first 
-                                                                      insts))
-                                                                      :test #'equal))
+                                                                (first 
+                                                                insts))
+                                                                :test #'equal))
                                                (getValue insts2 
                                                      insts2 
                                                      (second 
@@ -112,14 +119,29 @@
         (cons (write-to-string (first x)) (raw-mem (rest x)))
         nil))
 
-(defun lmc-load (file) (let ((lines (remove "" (getCleanLines 
-                                                    (with-open-file 
-                                                        (stream 
-                                                        file 
-                                                        :direction :input)
-                                                  (read-lines stream)))
-                                                  :test #'equal)))
-                            (clean-mem (to-mem (translate lines lines)))))
+(defun splitinstruction (inst)
+        (remove "" (split-sequence " " inst) :test 'equal))
+
+(defun validLines (lines)
+        (if lines
+         (and (validLine (first lines))
+              (validLines (rest lines)))
+         T))
+
+(defun validLine (line)
+        (let ((inst (splitinstruction line)))
+        (if (or (and (string= (first inst) "DAT")
+                 (eq (length inst) 1))
+                (and (eq (length inst) 2)
+                (or (and (string= (first inst) "DAT")
+                          (isValue (second inst)))
+                     (and (getOp (first inst))
+                          (isValidArg (second inst)))))
+                (and (eq (length inst) 3)
+                 (isLabel (first inst))
+                 (getOp (second inst))
+                 (isValidArg (third inst))))
+                T nil)))
 
 (defun getComponents (inst)
           (list (subseq inst 0 1)
@@ -134,6 +156,7 @@
             (cons (car l) (replace-elem a (1- n) (cdr l))))))
 
 (defun add (arg st)
+        (if (eq (mod (+ (getf (rest st) :pc) 1) 1000) 0) nil 
         (let ((roba (getf (rest st) :acc)))
         ;(write roba)
         (list   'state
@@ -145,9 +168,10 @@
                 :out (getf (rest st) :out)
                 :flag (if (> (+ (getf (rest st) :acc) 
                         (parse-integer (nth arg (getf (rest st) :mem)))) 
-                                999) 'flag 'noflag))))
+                                999) 'flag 'noflag)))))
 
 (defun sub (arg st)
+        (if (eq (mod (+ (getf (rest st) :pc) 1) 1000) 0) nil 
         (let ((roba (getf (rest st) :acc)))
         ;(write roba)
         (list   'state
@@ -159,9 +183,10 @@
                 :out (getf (rest st) :out)
                 :flag (if (<  (- (getf (rest st) :acc) 
                         (parse-integer (nth arg (getf (rest st) :mem))))
-                                0) 'flag 'noflag))))
+                                0) 'flag 'noflag)))))
 
 (defun store (arg st)
+        (if (eq (mod (+ (getf (rest st) :pc) 1) 1000) 0) nil 
         (list   'state
                 :acc (getf (rest st) :acc)
                 :pc (mod (+ (getf (rest st) :pc) 1) 1000)
@@ -169,9 +194,10 @@
                         arg (getf (rest st) :mem))
                 :in (getf (rest st) :in)
                 :out (getf (rest st) :out)
-                :flag (getf (rest st) :flag)))
+                :flag (getf (rest st) :flag))))
 
 (defun load_ (arg st)
+        (if (eq (mod (+ (getf (rest st) :pc) 1) 1000) 0) nil 
         (list   'state
                 :acc (if (>= arg 0)
                         (parse-integer (nth arg (getf (rest st) :mem)))
@@ -180,7 +206,7 @@
                 :mem (getf (rest st) :mem)
                 :in (getf (rest st) :in)
                 :out (getf (rest st) :out)
-                :flag (getf (rest st) :flag)))
+                :flag (getf (rest st) :flag))))
 
 (defun branch (arg st)
         (list   'state
@@ -191,24 +217,27 @@
                 :out (getf (rest st) :out)
                 :flag (getf (rest st) :flag)))
 
+;FIX
 (defun branchiz (arg st)
         (list   'state
                 :acc (getf (rest st) :acc)
                 :pc (if (and (eq (getf (rest st) :flag) 'noflag)
                              (eq (getf (rest st) :acc) 0))
                             arg
-                                (mod (+ (getf (rest st) :pc) 1) 1000))
+                            (if (eq (mod (+ (getf (rest st) :pc) 1) 1000) 0)
+                                nil (mod (+ (getf (rest st) :pc) 1) 1000)))
                 :mem (getf (rest st) :mem)
                 :in (getf (rest st) :in)
                 :out (getf (rest st) :out)
                 :flag (getf (rest st) :flag)))
-
+;FIX
 (defun branchip (arg st)
         (list   'state
                 :acc (getf (rest st) :acc)
                 :pc (if (eq (getf (rest st) :flag) 'noflag)
                              arg
-                            (mod (+ (getf (rest st) :pc) 1) 1000))
+                            (if (eq (mod (+ (getf (rest st) :pc) 1) 1000) 0)
+                                nil (mod (+ (getf (rest st) :pc) 1) 1000)))
                 :mem (getf (rest st) :mem)
                 :in (getf (rest st) :in)
                 :out (getf (rest st) :out)
@@ -216,6 +245,7 @@
 
 ;;;ACC NULL IF ERR
 (defun input (st)
+        (if (eq (mod (+ (getf (rest st) :pc) 1) 1000) 0) nil 
         (list   'state
                 :acc (if (getf (rest st) :in)
                         (first (getf (rest st) :in))
@@ -225,16 +255,18 @@
                 :in (if (getf (rest st) :in) (rest (getf (rest st) :in)) 
                         (getf (rest st) :in))
                 :out (getf (rest st) :out)
-                :flag (getf (rest st) :flag)))
+                :flag (getf (rest st) :flag))))
 
 (defun output (st)
+        (if (eq (mod (+ (getf (rest st) :pc) 1) 1000) 0) nil 
         (list   'state
                 :acc (getf (rest st) :acc)
                 :pc (mod (+ (getf (rest st) :pc) 1) 1000)
                 :mem (getf (rest st) :mem)
                 :in (getf (rest st) :in)
-                :out (append (getf (rest st) :out) (list (getf (rest st) :acc)))
-                :flag (getf (rest st) :flag)))
+                :out (append (getf (rest st) :out) 
+                        (list (getf (rest st) :acc)))
+                :flag (getf (rest st) :flag))))
 
 (defun halt (st)
         (list   'halted_state
@@ -332,7 +364,8 @@
             (eq (getf (rest st) :pc) nil)
             (eq (getf (rest st) :mem) nil)
             (eq (getf (rest st) :flag) nil)
-            (> (+ (getf (rest st) :pc) 1) (length (getf (rest st) :mem)))) nil T)))
+            (> (+ (getf (rest st) :pc) 1) 
+                (length (getf (rest st) :mem)))) nil T)))
 
 (defun lmc-run (file in) 
     (execution-loop (list   'state
@@ -342,3 +375,14 @@
                             :in in
                             :out nil
                             :flag 'noflag)))
+
+(defun lmc-load (file) (let ((lines (remove "" (getCleanLines 
+                                                    (with-open-file 
+                                                        (stream 
+                                                        file 
+                                                        :direction :input)
+                                                  (read-lines stream)))
+                                                  :test #'equal)))
+                            (if (validLines lines)
+                                (clean-mem (to-mem (translate lines lines)))
+                                nil)))
